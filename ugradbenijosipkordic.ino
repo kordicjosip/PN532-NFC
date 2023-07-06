917799ea4683a6577fade84bdf5754d2fb7e5aeb
kordicjosip <65564287+kordicjosip@users.noreply.github.com>
2023-07-06 12:20:28 +0200

    Update ugradbenijosipkordic.ino

--- ugradbenijosipkordic.ino
+++ ugradbenijosipkordic.ino
@@ -32,19 +32,19 @@ void loop(void)
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
   //uint8_t uid1[] = { 4, 5A, 63, 4A, 1E, 5D, 80 };
   //uint8_t uid2[] = { 4, 87, 2F, 4A, 1E, 5D, 80 };
-  // UID size (4 or 7 bytes depending on card type)
+  // UID size (4 ili 7 bajtova ovisno o kartici)
   uint8_t uidLength;
 
   while (!connected) {
     connected = connect();
   }
 
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  // Čeka ISO14443A karticu. Kad pronađe
+  // varijabla 'uid' će se popuniti UID-om kartice i ispisati uidLength
+  // 4 byte-a - Mifare Classic ili 7 byteova Mifare Ultralight
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
-  // If the card is detected, print the UID
+  // Ako detektira karticu, ispiši UID
   if (success)
   {
     Serial.println("Card Detected");
@@ -95,7 +95,7 @@ void loop(void)
   }
   else
   {
-    // PN532 probably timed out waiting for a card
+    // PN532 vjerojatno time-out
     // Serial.println("Timed out waiting for a card");
   }
 }
@@ -104,7 +104,7 @@ bool connect() {
   
   nfc.begin();
 
-  // Connected, show version
+  // connected
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata)
   {
@@ -116,9 +116,9 @@ bool connect() {
   Serial.print("Firmware version: "); Serial.print((versiondata >> 16) & 0xFF, DEC);
   Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
 
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
+  // Postavi maksimalan broj pokušaja čitanja s kartice
+  // Ovo je potrebno jer je defaultno ponašanje PN532 da
+  // zauvijek čeka da nešto pročita s kartice
   nfc.setPassiveActivationRetries(0xFF);
 
   // configure board to read RFID tags
